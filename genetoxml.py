7b60371534a0a8de04366d221cae5da59bef1d09
Sori Kang <sorik@student.unimelb.edu.au>
2013-01-03 11:41:38 +1100

    Modified subprocess and re

--- genetoxml.py
+++ genetoxml.py
@@ -2,7 +2,6 @@
 
 from ftplib import FTP
 
-
 import glob
 import re
 import subprocess
@@ -20,8 +19,9 @@ def main():
     ftp = FTP('ftp.ncbi.nlm.nih.gov')
     ftp.login()
 
-    # get the last modified time for server
-    # the return would be '213 20120101051112'
+    # get the last modified time of server data
+    # the return value would be '213 20120101051112'
+    
     modifiedTime = ftp.sendcmd('MDTM ' + homo_sapiens)
     mdTimeResult = modifiedTime.split()
     
@@ -33,20 +33,23 @@ def main():
         return
 
     # get modified time of the files that were retrieved
+    
     mdFiles = []
-    files = glob.glob('./Homo_sapiens*')
+    files = glob.glob('./Homo_sapiens_*.ags.gz')
     for file in files:
-        m = re.match('.*Homo_sapiens_([0-9]*)\.ags.gz', file)
+        m = re.match('\.\/Homo_sapiens_([0-9]*)\.ags\.gz', file)
         mdFiles.append(int(m.group(1)))
 
     # compare modified time
     # if there is no files starting with 'Homo_sapiens' or
     # the file version is not the latest version, 
     # retrieve the latest version from the server.
+    
     mdFiles.sort()
     if len(mdFiles) == 0 or mdFiles[-1] < mdServer:
         storeFile = './Homo_sapiens_%s.ags.gz' % str(mdServer)
         f = open(storeFile, 'wb')
+
         def callback(data):
             f.write(data)
         
@@ -54,10 +57,9 @@ def main():
 	f.close()
         
         # convert file to xml
-        outFile = './Homo_sapiens_%s.xml' % str(mdServer)
-        subprocess.call('linux.gene2xml -i %s -o %s -c -b' % (storeFile, outFile),
-			shell=True)
-        
+        outFile = './Homo_sapiens_%s.xml' % str(mdServer)      
+        subprocess.call(['linux.gene2xml', '-i', storeFile,
+			 '-o', outFile, '-b', '-c'])
     else:
         print 'You have the lastest version. %s' % str(mdFiles[-1])
 
@@ -67,5 +69,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-
 
