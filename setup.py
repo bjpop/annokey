0a2f66203afb80376942e4b8d6d349297513a344
Bernie Pope <florbitous@gmail.com>
2014-10-28 17:08:05 +1100

    bumped minor version number to account for logging of organism

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='annokey',
-    version='1.0.1',
+    version='1.0.2',
     author='Bernie Pope',
     author_email='bjpope@unimelb.edu.au',
     packages=['annokey'],
